01182450942444cae399e6e64df2040d4902a8fe
Ivett Ördög <devillmeister@gmail.com>
2023-10-09 22:26:53 +0200

    Add some smart behaviour

--- Player.js
+++ Player.js
@@ -2,12 +2,32 @@ const GameState = require('./src/GameState');
 
 class Player {
   static get VERSION() {
-    return '0.1';
+    return 'Demo player';
   }
 
   static betRequest(gameState, bet) {
-    var game = new GameState(gameState);
-    bet(0);
+    let game = new GameState(gameState);
+
+    try {
+      switch (game.me().name()) {
+        case "Bluffy the Vampire":
+          bet(Math.random() < 0.25 ? 10000 : 0);
+          break;
+        case "Winnie the Pot":
+          bet(game.me().score() > 7 ? game.toRaise() : 0);
+          break;
+        case "Darth Raiser":
+          bet(game.me().score() > 8 || Math.random() < 0.05 ? game.toRaise() : 0);
+          break;
+        default:
+          bet(10000);
+          break;
+      }
+    }catch (e) {
+      console.log(e);
+      console.log('Fold back case');
+      bet(0);
+    }
   }
 
   static showdown(gameState) {
